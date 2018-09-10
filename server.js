d11279b1f57ff8704a9b2b5ca31304c455f710b6
Your Name <you@example.com>
2018-09-10 19:43:01 +0530

    Signed-off-by: Your Name <you@example.com>

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('connection', function (socket) {
  
+  console.log('user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  socket.on('add-message', (message) => {
+    io.emit('message', {type:'new-message', text: message});    
+  });
+ 
 });
 
 
