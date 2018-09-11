c7041abb75a8799c74c30f4a77a45d2bc0ea9674
Your Name <you@example.com>
2018-09-11 15:16:33 +0530

    Signed-off-by: Your Name <you@example.com>

--- server.js
+++ server.js
@@ -2,36 +2,27 @@ var express =require('express');
 var app = express();
 var port=process.env.PORT || 3000;
 //let http = require('http').Server(app);
-var server  = require('http').createServer(app);
+var server  = require('http').createServer(app).listen(port);;
 
-var io      = require('socket.io')(server);
+var io      = require('socket.io');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get(/^socket.io.js$/, function(req, res) {
 
-  res.setHeader('Access-Control-Allow-Origin', 'TRUE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
-  res.sendfile('socket.io.js');
-  next();
-  
-  });
 
-io.sockets.on('connection', function (socket) {
+// io.sockets.on('connection', function (socket) {
  
-  console.log('user connected');
+  // console.log('user connected');
   
-   socket.on('disconnect', function(){
-     console.log('user disconnected');
-   });
+   // socket.on('disconnect', function(){
+     // console.log('user disconnected');
+   // });
   
-   socket.on('add-message', function(message) {
-     socket.emit('message', {type:'new-message', text: message});    
-   });
+   // socket.on('add-message', function(message) {
+     // socket.emit('message', {type:'new-message', text: message});    
+   // });
  
-});
+// });
 
 
 
@@ -39,7 +30,7 @@ io.sockets.on('connection', function (socket) {
  console.log('hello from server');
  res.render('./public/index.html');
  });
-server.listen(port);
+//server.listen(port);
 
 console.log('Server Listening at port'+port);
 
