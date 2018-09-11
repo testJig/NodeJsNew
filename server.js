002989bbd0e69909cad3f6cad301279066184c5d
Your Name <you@example.com>
2018-09-11 10:04:19 +0530

    Signed-off-by: Your Name <you@example.com>

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var server  = require('http').createServer(app);
 
 var io      = require('socket.io')(server);
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get(/^socket.io.js$/, function(req, res) {
 
