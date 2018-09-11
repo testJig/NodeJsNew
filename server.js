c528dc9f2b295fd118095a2621a4b6d971590e99
Your Name <you@example.com>
2018-09-11 11:12:30 +0530

    Signed-off-by: Your Name <you@example.com>

--- server.js
+++ server.js
@@ -44,13 +44,4 @@ server.listen(port);
 
 console.log('Server Listening at port'+port);*/
 
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
+let app = require('express')();
