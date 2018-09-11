023e1161674aedce106b1d5b0edc0cb36c834de5
Your Name <you@example.com>
2018-09-11 09:41:36 +0530

    Signed-off-by: Your Name <you@example.com>

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express =require('express');
 var app = express();
 var port=process.env.PORT || 3000;
-let http = require('http').Server(app);
+//let http = require('http').Server(app);
 var server  = require('http').createServer(app);
 
-var io      = require('socket.io').listen(server);
+var io      = require('socket.io')(server);
 
 //app.use(express.static(__dirname + '/public'));
 
